078ac6ae329a2f22962db028b633cb3f0aac6b56
luka <lukakurdadze2@gmail.com>
2022-05-29 21:31:09 +0400

    fix: table layout

--- resources/views/components/table-row.blade.php
+++ resources/views/components/table-row.blade.php
@@ -1,10 +1,12 @@
 @props(['location', 'new_cases' => '-', 'death' => '-', 'recovered' => '-'])
 <tr>
-    <td x-data="if (window.innerWidth < 700) {open = false}
-                else {open = true}"
+    <td x-data="{ open: undefined }"
+        x-init="width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+                        if (width < 700) {open = false}
+                        else {open = true}"
         @resize.window="width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-                        if (width < 700) open = false
-                        else open = true"
+                        if (width < 700) {open = false}
+                        else {open = true}"
         class="whitespace-nowrap py-4 pl-4 pr-3 font-medium text-gray-900 text-xs md:text-base lg:text-xl sm:pl-6">
         <div x-show="open">{{ $location }}</div>
         <div x-show="!open">{!! str_replace(" ", '<br>', $location) !!}</div>
