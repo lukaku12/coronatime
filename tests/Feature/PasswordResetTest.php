37b8f332a3ff9b90d31ff6885836246584029aa5
luka <lukakurdadze2@gmail.com>
2022-06-03 01:44:43 +0400

    test: passwordResetTest 100% coverage

--- tests/Feature/PasswordResetTest.php
+++ tests/Feature/PasswordResetTest.php
@@ -3,9 +3,10 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Auth\Notifications\ResetPassword;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class PasswordResetTest extends TestCase
@@ -19,53 +20,84 @@ class PasswordResetTest extends TestCase
 		$response->assertStatus(200);
 	}
 
-//	public function test_reset_password_link_can_be_requested()
-//	{
-//		Notification::fake();
-//
-//		$user = User::factory()->create();
-//
-//		$this->post('/forget-password', ['email' => $user->email]);
-//
-//		Notification::assertSentTo($user, ResetPassword::class);
-//	}
-//
-//	public function test_reset_password_screen_can_be_rendered()
-//	{
-//		Notification::fake();
-//
-//		$user = User::factory()->create();
-//
-//		$this->post('/forget-password', ['email' => $user->email]);
-//
-//		Notification::assertSentTo($user, ResetPassword::class, function ($notification) {
-//			$response = $this->get('/reset-password/' . $notification->token);
-//
-//			$response->assertStatus(200);
-//
-//			return true;
-//		});
-//	}
-//
-//	public function test_password_can_be_reset_with_valid_token()
-//	{
-//		Notification::fake();
-//
-//		$user = User::factory()->create();
-//
-//		$this->post('/forget-password', ['email' => $user->email]);
-//
-//		Notification::assertSentTo($user, ResetPassword::class, function ($notification) use ($user) {
-//			$response = $this->post('/reset-password', [
-//				'token'                 => $notification->token,
-//				'email'                 => $user->email,
-//				'password'              => 'password',
-//				'password_confirmation' => 'password',
-//			]);
-//
-//			$response->assertSessionHasNoErrors();
-//
-//			return true;
-//		});
-//	}
+	public function test_show_error_massage_if_user_does_not_enter_valid_email()
+	{
+		$this->withExceptionHandling();
+		$response = $this->post('/forget-password', ['test@gmail.com']);
+		$response->assertSessionHasErrors(['email']);
+	}
+
+	public function test_send_email_to_user_if_email_is_valid()
+	{
+		$this->withExceptionHandling();
+		$user = User::factory()->create(['email' => 'lukakurdadze2@gmail.com']);
+		$response = $this->post('/forget-password', ['email' => $user->email]);
+		$response->assertSessionHasNoErrors();
+	}
+
+	public function test_show_reset_password_page_after_coming_from_mail()
+	{
+		$this->withExceptionHandling();
+
+		$token = Str::random(64);
+
+		$response = $this->get('/reset-password/{token}', ['token' => $token]);
+
+		$response->assertStatus(200);
+	}
+
+	public function test_show_error_massage_if_users_entered_passwords_do_not_match()
+	{
+		$this->withExceptionHandling();
+		$response = $this->post('/reset-password', ['password' => 'test12345', 'password_confirmation' => 'test']);
+		$response->assertSessionHasErrors(['password']);
+	}
+
+	public function test_throw_validation_error_if_token_is_invalid()
+	{
+		$this->withExceptionHandling();
+
+		$user = User::factory()->create();
+
+		$this->session(['email' => $user->email]);
+
+		$token = Str::random(64);
+
+		DB::table('password_resets')->insert([
+			'email'      => $user->email,
+			'token'      => $token,
+			'created_at' => Carbon::now(),
+		]);
+
+		$response = $this->post('/reset-password', [
+			'password'              => $user->password,
+			'password_confirmation' => $user->password,
+			'token'                 => 'THIS IS INVALID TOKEN',
+		]);
+		$response->assertSessionHasErrors(['password_confirmation']);
+	}
+
+	public function test_update_password_if_all_credentials_are_valid()
+	{
+		$this->withExceptionHandling();
+
+		$user = User::factory()->create();
+
+		$this->session(['email' => $user->email]);
+
+		$token = Str::random(64);
+
+		DB::table('password_resets')->insert([
+			'email'      => $user->email,
+			'token'      => $token,
+			'created_at' => Carbon::now(),
+		]);
+
+		$response = $this->post('/reset-password', [
+			'password'              => $user->password,
+			'password_confirmation' => $user->password,
+			'token'                 => $token,
+		]);
+		$response->assertStatus(200);
+	}
 }
